0b4a588ccb27d0d76b41288684d5a3ddf68ce307
theaharris <supershay84@gmail.com>
2021-01-20 16:44:57 -0500

    Fixed albums

--- record-collection-api/app/controllers/albums_controller.rb
+++ record-collection-api/app/controllers/albums_controller.rb
@@ -5,12 +5,12 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.all
 
-    render json: @albums
+    render json: @albums.to_json(include: [:artists, :genres, :songs])
   end
 
   # GET /albums/1
   def show
-    render json: @album
+    render json: @album.to_json(include: [:artists, :genres, :songs])
   end
 
   # POST /albums
